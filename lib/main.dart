41ea4a3f07afcdcdd29239c832a31de0f50a7791
Abdul Aziz Ahwan <abdulazizahwan97@students.unnes.ac.id>
2019-05-20 10:49:14 +0700

    mengembalikan ukuran lebar

--- lib/main.dart
+++ lib/main.dart
@@ -107,9 +107,7 @@
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  MyApp()
-);
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -117,24 +115,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-         backgroundColor: Colors.pink,
-          title: Text('Specimen'),
-        ),
-        body: Center(
-          child: Container(
-          height: 300,
-          width: 400,
-          alignment: Alignment(0.9, -0.98),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            image: DecorationImage(
-              image: NetworkImage('https://cdn.dribbble.com/users/2132553/screenshots/5083276/dribbble_16.png'),fit: BoxFit.cover
-            )
+          appBar: AppBar(
+            backgroundColor: Colors.pink,
+            title: Text('Specimen'),
           ),
-        ),
-        )
-      ),
+          body: Center(
+            child: Container(
+              height: 300,
+              width: 300,
+              alignment: Alignment(0.9, -0.98),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          'https://cdn.dribbble.com/users/2132553/screenshots/5083276/dribbble_16.png'),
+                      fit: BoxFit.cover)),
+            ),
+          )),
     );
   }
 }
