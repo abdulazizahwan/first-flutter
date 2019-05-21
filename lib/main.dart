1d2ace05299b74e282618d36c050931ac48291e5
abdulazizahwan <abdulazizahwan97@students.unnes.ac.id>
2019-05-21 19:56:41 +0700

    menambahkan circle avatar

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,20 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         home: Scaffold(
           appBar: AppBar(
-            backgroundColor: Colors.pink,
+            backgroundColor: Colors.blue,
             title: Text('Testing'),
           ),
+          backgroundColor: Colors.orange,
           body: Row(
-            children: <Widget>[Icon(Icons.mail_outline), Text("Mail")],
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: new NetworkImage(
+                    'https://miro.medium.com/max/3150/1*nZcJuem54HxxYvEf0VysQw.jpeg'),
+                backgroundColor: Colors.blue,
+                radius: 32.0,
+              ),
+              Text("Aziz Ahwan", style: TextStyle(fontSize: 24)),
+            ],
           ),
         ));
   }
