fd6f38586d237260771a7f139599e652874fa538
abdulazizahwan <abdulazizahwan97@students.unnes.ac.id>
2019-05-21 17:09:59 +0700

    menghapus kode

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,8 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.pink,
             title: Text('Testing'),
           ),
-          body: Center(
-            child: Container(
-              height: 300,
-              width: 300,
-              alignment: Alignment(0.9, -0.98),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  image: DecorationImage(
-                      image: NetworkImage(
-                          'https://cdn.dribbble.com/users/2132553/screenshots/5083276/dribbble_16.png'),
-                      fit: BoxFit.cover)),
-            ),
-          )),
+          body: 
+          ),
     );
   }
 }
